214192bd3452836099de8c1476e8bb7ba4835b11
enrico <ezanardo@onezerobinary.com>
2020-05-27 16:21:31 +0200

    add counter labels into React App

--- src/sc/SocialCounter.tsx
+++ src/sc/SocialCounter.tsx
@@ -6,11 +6,24 @@ import '@tensorflow/tfjs';
 import './SocialCounter.css';
 
 const MAX_VALUE = 5000;
-const DELAY = 1000; //ms
+const DELAY = 100; //ms
+const CYCLE = 2000; //ms
 
-type Data = {
-  [timestamp: string]: Person[];
-};
+const WIDTH = 480;
+const HEIGHT = 360;
+const LINE = WIDTH / 2; // border from left/right in pixel
+
+enum Direction {
+  'left2right' = 'left2right',
+  'right2left' = 'right2left',
+  'Up2Down' = 'Up2Down',
+  'Down2Up' = 'Down2Up',
+}
+
+enum Position {
+  'LEFT' = 'LEFT',
+  'RIGHT' = 'RIGHT',
+}
 
 type mm = number;
 type pixels = number;
@@ -22,22 +35,28 @@ type Centroid = {
 
 type Person = {
   id: string;
-  point: Centroid;
+  centroid: Centroid;
   predictionPoint: Centroid;
   prediction: cocoSsd.DetectedObject;
+  position: Position;
 };
+
+type Count = { left: number; right: number };
+const initialInfo: { [timestamp: string]: Count } = {};
 
 export const SocialCounter: React.FC = () => {
   let detectedPeople: Person[] = [];
 
-  let peopleCounter = 0;
-  let leftCounter = 0;
-  let rightCounter = 0;
+  //TODO: Init the DB...
+  const [leftCounter, setLeftCounter] = useState(0);
+  const [rightCounter, setRightCounter] = useState(0);
+  const [peopleCounter, setPeopleCounter] = useState(0);
 
-  const data: Data = {};
+  const [info, setInfo] = useState(initialInfo);
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
   // When the component will mount
   useEffect(() => {
     async function fetchData() {
@@ -52,11 +71,11 @@ export const SocialCounter: React.FC = () => {
     });
   }, []);
 
-  //TODO: Store in some DB that info
-  function storeObjects(person: Person) {
-    const timestamp = new Date().valueOf();
-    data[timestamp].push(person);
-    // console.log(data);
+  useEffect(() => updateInfo(), [info]);
+
+  function updateInfo() {
+    console.log(`##################################`);
+    // var counter = info[0] ? `<p>Yes ${info[0].left} </p>` : '<p>No</p>';
   }
 
   function calculatePredictionPoint(
@@ -68,25 +87,27 @@ export const SocialCounter: React.FC = () => {
       y: 0,
     };
 
-    let deltaX = Math.abs(person.point.x - centroid.x) * 2;
-    let deltaY = Math.abs(person.point.y - centroid.y) * 2;
+    let deltaX = Math.abs(person.centroid.x - centroid.x) * 2;
+    let deltaY = Math.abs(person.centroid.y - centroid.y) * 2;
 
     // Define direction
     let horizontalDirection =
-      person.point.x <= centroid.x ? 'left2right' : 'right2left';
+      person.centroid.x <= centroid.x
+        ? Direction.left2right
+        : Direction.right2left;
     let verticalDirection =
-      person.point.y <= centroid.y ? 'Up2Down' : 'Down2Up';
+      person.centroid.y <= centroid.y ? Direction.Up2Down : Direction.Down2Up;
 
-    if (horizontalDirection === 'left2right') {
-      predictionPoint.x = person.point.x + deltaX;
+    if (horizontalDirection === Direction.left2right) {
+      predictionPoint.x = person.centroid.x + deltaX;
     } else {
-      predictionPoint.x = person.point.x - deltaX;
+      predictionPoint.x = person.centroid.x - deltaX;
     }
 
-    if (verticalDirection === 'Down2Up') {
-      predictionPoint.y = person.point.y + deltaY;
+    if (verticalDirection === Direction.Down2Up) {
+      predictionPoint.y = person.centroid.y + deltaY;
     } else {
-      predictionPoint.y = person.point.y - deltaY;
+      predictionPoint.y = person.centroid.y - deltaY;
     }
 
     return predictionPoint;
@@ -97,7 +118,7 @@ export const SocialCounter: React.FC = () => {
     centroid: Centroid
   ): pixels {
     let distance = Math.pow(
-      Math.pow(personCentroid.x - centroid.x, 2) +
+      Math.pow(Math.abs(personCentroid.x - centroid.x), 2) +
         Math.pow(personCentroid.y - centroid.y, 2),
       0.5
     );
@@ -113,10 +134,9 @@ export const SocialCounter: React.FC = () => {
       }
 
       const modelPromise = await cocoSsd.load();
-      // await checkFrame(modelPromise);
-
-      // check again after 5 seconds
-      setInterval(async () => await checkFrame(modelPromise), 5000);
+      // check every n seconds
+      // setInterval(async () => await checkFrame(modelPromise), CYCLE);
+      setInterval(async () => await checkCounter(modelPromise), CYCLE);
 
       try {
         resolve(true);
@@ -142,107 +162,189 @@ export const SocialCounter: React.FC = () => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async function checkFrame(
+  // Main function
+  async function checkCounter(
     modelPromise: cocoSsd.ObjectDetection
-  ): Promise<cocoSsd.DetectedObject[]> {
-    if (videoRef.current) {
-      // Reset Counters
-      peopleCounter = 0;
-      detectedPeople = [];
+  ): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        // Reset Counters
+        detectedPeople = [];
+        setLeftCounter(0);
+        setRightCounter(0);
 
-      // Objects Detector
-      let predictions = await modelPromise.detect(videoRef.current);
+        // DetectPeople
+        detectedPeople = await detectPeople(modelPromise);
 
-      // Populate detectedPeople
-      predictions.map((prediction: cocoSsd.DetectedObject) => {
-        if (prediction.class === 'person') {
-          //Calculate centroid
-          let centroid = calculateCentroid(prediction);
+        console.log(`Detected People: ${detectedPeople.length}`);
 
-          const person: Person = {
-            id: `Id ${peopleCounter + 1}`.toString(),
-            point: centroid,
-            predictionPoint: { x: 0, y: 0 },
-            prediction: prediction,
-          };
+        // Fetch new data after a delay
+        console.log(`Wait ${DELAY * 0.001} seconds`);
+        await delay(DELAY);
+        await addPredictionPointsToDetetedPeople(modelPromise);
 
-          detectedPeople.push(person);
-          console.log(detectedPeople.length);
-        }
-      });
+        // Verify Predictions
+        await verifyPredictions(modelPromise);
 
-      // Predict Position
-      // Wait 1 sec.
-      await delay(DELAY);
-      console.log(`After ${DELAY * 0.001} seconds`);
-      predictions = await modelPromise.detect(videoRef.current);
+        let count: Count = {
+          left: leftCounter,
+          right: rightCounter,
+        };
 
-      predictions.map((prediction: cocoSsd.DetectedObject) => {
-        if (prediction.class === 'person') {
-          //Calculate centroid
-          let centroid = calculateCentroid(prediction);
+        await save(count);
 
-          // Detect the nearest Point
-          if (detectedPeople.length > 0) {
-            let minDistance = MAX_VALUE;
-            detectedPeople.map((person: Person) => {
-              let distance = calculateEuclideanDistance(person.point, centroid);
-              if (distance < minDistance) {
-                minDistance = distance;
-                // Associate this centroid as Prediction Point to the Person
-                person.predictionPoint = calculatePredictionPoint(
-                  person,
-                  centroid
-                );
-                console.log(person.predictionPoint);
+        // Render Predictions
+        renderPredictions(detectedPeople);
+        // requestAnimationFrame(() => {
+        //   checkFrame(modelPromise);
+        // });
+
+        resolve(true);
+      } catch (error) {
+        reject(`An error on the counter was catched. ${error}`);
+      }
+    });
+  }
+
+  async function verifyPredictions(
+    modelPromise: cocoSsd.ObjectDetection
+  ): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (videoRef.current) {
+          let predictions = await modelPromise.detect(videoRef.current);
+
+          predictions.map((prediction: cocoSsd.DetectedObject) => {
+            if (prediction.class === 'person') {
+              //Calculate centroid
+              let centroid = calculateCentroid(prediction);
+              let minDistance = MAX_VALUE;
+
+              // Detect the nearest Point
+              if (detectedPeople.length > 0) {
+                detectedPeople.map((person: Person) => {
+                  let distance = calculateEuclideanDistance(
+                    person.predictionPoint,
+                    centroid
+                  );
+                  if (distance < minDistance) {
+                    minDistance = distance;
+                    // Associate this person to this path
+                    person.prediction = prediction;
+                    person.centroid = centroid;
+                    person.position = calculatePosition(centroid.x);
+
+                    // Update the counters
+                    updateCounter(person.position);
+
+                    console.log(person.id);
+                    console.log(
+                      `New position for ${person.id} found - ${person.centroid.x} - ${person.centroid.y}`
+                    );
+                  }
+                });
               }
-            });
-          }
+            }
+          });
         }
-      });
+        resolve(true);
+      } catch (error) {
+        reject(`It was not possible to verify the Prediction. ${error}`);
+      }
+    });
+  }
 
-      // Verify
-      // Wait 1 sec.
-      await delay(DELAY);
-      console.log(`After ${DELAY * 0.001} seconds`);
-      predictions = await modelPromise.detect(videoRef.current);
+  async function addPredictionPointsToDetetedPeople(
+    modelPromise: cocoSsd.ObjectDetection
+  ): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (videoRef.current) {
+          let predictions = await modelPromise.detect(videoRef.current);
 
-      predictions.map((prediction: cocoSsd.DetectedObject) => {
-        if (prediction.class === 'person') {
-          //Calculate centroid
-          let centroid = calculateCentroid(prediction);
+          predictions.map((prediction: cocoSsd.DetectedObject) => {
+            if (prediction.class === 'person') {
+              //Calculate centroid
+              let centroid = calculateCentroid(prediction);
 
-          // Detect the nearest Point
-          if (detectedPeople.length > 0) {
-            let minDistance = MAX_VALUE;
-            detectedPeople.map((person: Person) => {
-              let distance = calculateEuclideanDistance(
-                person.predictionPoint,
-                centroid
-              );
-              if (distance < minDistance) {
-                minDistance = distance;
-                // Associate this person to this path
-                person.prediction = prediction;
-                person.point = calculateCentroid(prediction);
-                console.log(person.id);
-                console.log(`${person.point.x} - ${person.point.y}`);
+              let minDistance = MAX_VALUE;
+
+              // Detect the nearest Point
+              if (detectedPeople.length > 0) {
+                detectedPeople.map((person: Person) => {
+                  let distance = calculateEuclideanDistance(
+                    person.centroid,
+                    centroid
+                  );
+
+                  if (distance < minDistance) {
+                    minDistance = distance;
+                    // Associate this centroid as Prediction Point to the Person
+                    person.predictionPoint = calculatePredictionPoint(
+                      person,
+                      centroid
+                    );
+                    console.log(`New Prediction for ${person.id} found`);
+                  }
+                });
               }
-            });
-          }
+            }
+          });
         }
-      });
+        resolve(true);
+      } catch (error) {
+        reject(
+          `It was not possible to calculate the prediction points for the People. ${error}`
+        );
+      }
+    });
+  }
 
-      // storeCounterData(peopleCounter);
-
-      renderPredictions(detectedPeople);
-      // requestAnimationFrame(() => {
-      //   checkFrame(modelPromise);
-      // });
-
-      return predictions;
+  function calculatePosition(x: number): Position {
+    let position = Position.RIGHT;
+    if (x < LINE) {
+      position = Position.LEFT;
     }
-    return Promise.reject(console.log(`Is not possible to detect the frame.`));
+
+    return position;
+  }
+
+  async function detectPeople(
+    modelPromise: cocoSsd.ObjectDetection
+  ): Promise<Person[]> {
+    return new Promise(async (resolve, reject) => {
+      // Objects Detector
+      try {
+        if (videoRef.current) {
+          setPeopleCounter(0);
+          let predictions = await modelPromise.detect(videoRef.current);
+
+          // Populate detectedPeople
+          predictions.map((prediction: cocoSsd.DetectedObject) => {
+            if (prediction.class === 'person') {
+              //Calculate centroid
+              let centroid = calculateCentroid(prediction);
+              setPeopleCounter(peopleCounter + 1);
+
+              const person: Person = {
+                id: `Id${peopleCounter}`.toString(),
+                centroid: centroid,
+                predictionPoint: { x: 0, y: 0 },
+                prediction: prediction,
+                position: calculatePosition(centroid.x),
+              };
+
+              detectedPeople.push(person);
+              console.log(`Person added ${person.id}`);
+            }
+          });
+
+          resolve(detectedPeople);
+        }
+      } catch (error) {
+        reject(`It was not possible to detect People. ${error}`);
+      }
+    });
   }
 
   function calculateCentroid(prediction: cocoSsd.DetectedObject): Centroid {
@@ -259,6 +361,29 @@ export const SocialCounter: React.FC = () => {
     return centroid;
   }
 
+  function updateCounter(position: Position) {
+    if (position === Position.LEFT) {
+      setLeftCounter(leftCounter + 1);
+    } else {
+      setRightCounter(rightCounter + 1);
+    }
+  }
+
+  //TODO: Store in some DB that info
+  async function save(count: Count): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      try {
+        const timestamp = new Date().valueOf().toString();
+        let tmp = info;
+        tmp[timestamp] = count;
+        setInfo({ ...tmp });
+        resolve(true);
+      } catch (error) {
+        reject(`It was not possible to save the data. ${error}`);
+      }
+    });
+  }
+
   const renderPredictions = (detectedPeople: Person[]) => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
@@ -269,11 +394,11 @@ export const SocialCounter: React.FC = () => {
 
         // Line In-Out
         ctx.beginPath();
-        ctx.moveTo(240, 0);
-        ctx.lineTo(239, 0);
-        ctx.lineTo(239, 360);
-        ctx.lineTo(241, 360);
-        ctx.lineTo(241, 0);
+        ctx.moveTo(LINE, 0);
+        ctx.lineTo(LINE - 1, 0);
+        ctx.lineTo(LINE - 1, 360);
+        ctx.lineTo(LINE + 1, 360);
+        ctx.lineTo(LINE + 1, 0);
         ctx.fillStyle = 'yellow';
         ctx.fill();
 
@@ -281,7 +406,6 @@ export const SocialCounter: React.FC = () => {
         const font = 'bold 14px Arial';
         ctx.font = font;
         ctx.textBaseline = 'top';
-        const textHeight = parseInt(font, 10);
 
         detectedPeople.forEach((person: Person) => {
           const x = person.prediction.bbox[0];
@@ -289,43 +413,46 @@ export const SocialCounter: React.FC = () => {
           const width = person.prediction.bbox[2]; // ctx.canvas.width;
           const height = person.prediction.bbox[3]; // ctx.canvas.height;
 
+          console.log(`LeftCounter: ${leftCounter}`);
+          console.log(`RightCounter: ${rightCounter}`);
+
           // Draw the bounding box.
           ctx.strokeStyle = '#575756';
           ctx.lineWidth = 2;
           ctx.strokeRect(x, y, width, height);
           // Draw the label background.
-
-          ctx.fillStyle = '#009ee3';
-          const textWidth = ctx.measureText(person.id).width;
-          ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-        });
-
-        detectedPeople.forEach((person: Person) => {
-          const x = person.prediction.bbox[0] + 5;
-          const y = person.prediction.bbox[1] + 5;
           const y_score = y + 15;
-          // Draw the text last to ensure it's on top.
           ctx.fillStyle = '#009ee3';
           const score = person.prediction.score.toPrecision(3).toString();
-          ctx.fillText(person.id, x, y);
-          ctx.fillText(score, x, y_score);
+          ctx.fillText(person.id, x + 5, y + 2);
+          ctx.fillText(score, x + 5, y_score);
         });
       }
     }
   };
 
   return (
-    <div className="Main2">
-      <video
-        className="Video"
-        autoPlay
-        playsInline
-        muted
-        ref={videoRef}
-        width="480"
-        height="360"
-      />
-      <canvas className="Canvas" ref={canvasRef} width="480" height="360" />
+    <div>
+      <p>
+        Left: {leftCounter} - Right: {rightCounter}
+      </p>
+      <div className="Main2">
+        <video
+          className="Video"
+          autoPlay
+          playsInline
+          muted
+          ref={videoRef}
+          width={WIDTH}
+          height={HEIGHT}
+        />
+        <canvas
+          className="Canvas"
+          ref={canvasRef}
+          width={WIDTH}
+          height={HEIGHT}
+        />
+      </div>
     </div>
   );
 };
